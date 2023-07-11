102ebdcac6a12baf8b055416bb359e0fedf031fc
stemmy287 <steem287@gmail.com>
2023-07-11 17:56:25 +0300

    feat: custom input

---
+++ src/common/comonents/Input/Input.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Text, TextInput, View} from 'react-native';
+import {styles} from './InputStyles';
+
+type PropsType = {
+	title?: string;
+	value: string;
+	onChange: (e: any) => void;
+	onBlur: () => void;
+	password?: boolean;
+};
+
+export const Input = ({title, value, onChange, onBlur, password}: PropsType) => {
+	return (
+		<View>
+			{title && <Text style={styles.title}>{title}</Text>}
+			<TextInput
+				style={styles.input}
+				value={value}
+				onChangeText={onChange}
+				onBlur={onBlur}
+				secureTextEntry={password}
+			/>
+		</View>
+	);
+};
---
+++ src/common/comonents/Input/InputStyles.ts
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+	title: {
+		fontSize: 14,
+		fontWeight: '400',
+		lineHeight: 24,
+		color: '#aaadb2',
+	},
+	input: {
+		borderWidth: 1,
+		borderStyle: 'solid',
+		borderColor: '#c3c3c5',
+		borderRadius: 10,
+		width: 300,
+		height: 50,
+		color: '#46484c',
+		fontSize: 20,
+		paddingLeft: 10,
+		backgroundColor: '#fff',
+	},
+});
