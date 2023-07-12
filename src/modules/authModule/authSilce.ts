d478cfec8878f24383efac3062b6775c57ca0a4c
stemmy287 <steem287@gmail.com>
2023-07-12 13:33:39 +0300

    feat: auth module/api

---
+++ src/modules/authModule/authSilce.ts
@@ -0,0 +1,42 @@
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {LoginType, UserType} from './types';
+import {authApi} from './authApi';
+
+export const login = createAsyncThunk('auth/login', async (param: LoginType, {rejectWithValue}) => {
+	try {
+		const res = await authApi.login(param);
+		console.log(res.headers!.uid);
+		if (res.ok) {
+			return {
+				user: {
+					username: res.data!.user.username,
+					avatar_url: res.data!.user.avatar_url,
+				},
+				headerData: {
+					'access-token': res.headers!['access-token'],
+					client: res.headers!.client,
+					uid: res.headers!.uid,
+				},
+			};
+		} else {
+			return rejectWithValue(res.data);
+		}
+	} catch (e) {
+		return rejectWithValue(e);
+	}
+});
+
+const slice = createSlice({
+	name: 'auth',
+	initialState: {
+		user: {} as UserType,
+	},
+	reducers: {},
+	extraReducers: builder => {
+		builder.addCase(login.fulfilled, (state, action) => {
+			state.user = action.payload.user;
+		});
+	},
+});
+
+export const authReducer = slice.reducer;
