9e086c891235c2ff6f1514f95e2534af8ca5d3f6
stemmy287 <steem287@gmail.com>
2023-07-13 11:46:40 +0300

    feat: news module/selectors

---
+++ src/modules/newsModule/newsSelectors.ts
@@ -0,0 +1,4 @@
+import {AppRootStateType} from '../../store';
+
+export const newsSelector = (state: AppRootStateType) => state.news.news;
+export const newsItemSelector = (state: AppRootStateType) => state.news.newsItem;
