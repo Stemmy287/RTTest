d61e586501c91eb8a595c65c2f5881eefe629eda
stemmy287 <steem287@gmail.com>
2023-07-13 15:25:41 +0300

    fix: header data

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -1,17 +1,14 @@
 import {createSlice} from '@reduxjs/toolkit';
-import {HeaderDataType} from './types';
 import {login, UserType} from '../modules/authModule';
 
 const slice = createSlice({
 	name: 'app',
 	initialState: {
 		user: {} as UserType,
-		headerData: {} as HeaderDataType,
 	},
 	reducers: {},
 	extraReducers: builder => {
 		builder.addCase(login.fulfilled, (state, action) => {
-			state.headerData = action.payload.headerData;
 			state.user = action.payload.user;
 		});
 	},
--- src/modules/authModule/authSilce.ts
+++ src/modules/authModule/authSilce.ts
@@ -1,20 +1,21 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import {LoginType} from './types';
 import {authApi} from './authApi';
+import {instance} from '../../common/constants';
 
 export const login = createAsyncThunk('auth/login', async (param: LoginType, {rejectWithValue}) => {
 	try {
 		const res = await authApi.login(param);
 		if (res.ok) {
+			instance.setHeaders({
+				'access-token': res.headers!['access-token'],
+				client: res.headers!.client,
+				uid: res.headers!.uid,
+			});
 			return {
 				user: {
 					username: res.data!.user.username,
 					avatar_url: res.data!.user.avatar_url,
-				},
-				headerData: {
-					'access-token': res.headers!['access-token'],
-					client: res.headers!.client,
-					uid: res.headers!.uid,
 				},
 			};
 		}
